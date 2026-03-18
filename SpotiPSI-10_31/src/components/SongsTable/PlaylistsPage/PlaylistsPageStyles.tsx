54dc809c34cc3b72cc9fe43a1a1fa3228d3c8855
31-sudo <idokatav1@gmail.com>
2026-03-18 13:52:36 +0200

    Playlist page design

--- SpotiPSI-10_31/src/components/SongsTable/PlaylistsPage/PlaylistsPageStyles.tsx
+++ SpotiPSI-10_31/src/components/SongsTable/PlaylistsPage/PlaylistsPageStyles.tsx
@@ -27,6 +27,53 @@ const useStyles = makeStyles()(() => ({
     },
     allPlaylistsTitle: {
         padding: '25px'
+    },
+    button: {
+        color: '#9657B0',
+        border: '1px solid #9657B0',
+        borderRadius: '20px',
+        margin: '25px'
+    },
+    cancelButtonColor: {
+        color: '#9657B0'
+    },
+    createButtonColor: {
+        color: 'white'
+    },
+    textLabel: {
+        '& label': {
+            color: 'white',
+        },
+        '& label.Mui-focused': {
+            color: 'white',
+        },
+        '& .MuiInput-underline:before': {
+            borderBottomColor: 'white',
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: 'white',
+        },
+    },
+    dialog: {
+        direction: 'rtl',
+        '& .MuiDialog-container': {
+            '& .MuiPaper-root': {
+                width: '25%',
+                color: 'white',
+                backgroundColor: '#313131'
+            }
+        }
+    },
+    playlist: {
+        paddingLeft: '25px',
+        display: 'flex',
+        borderBottom: '1px solid #545454',
+        flexDirection: 'column',
+        alignItems: 'flex-end'
+    },
+    songsNumber: {
+        color: '#B3B3B3',
+        fontSize: '15px'
     }
 }));
 
